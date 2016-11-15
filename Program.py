99540a48ac8f9aa317fea5e4d88da8f2685b466c
Daniel Wotton <dwotton@aklys.net>
2016-11-15 20:00:08 +1100

    Junk Initial Code

--- Program.py
+++ Program.py
@@ -1,1 +1,12 @@
-print("This is a test")
+import collections
+
+def main():
+    print(search_details()[1])
+
+def search_details():
+    search_history = collections.OrderedDict()
+    search_history[input("What keyword do you want to look for? ")] = input("What location do you wish to search? ")
+    return search_history
+
+if __name__ == '__main__':
+    main()
